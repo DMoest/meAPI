9585f14b91d22fd28397c077df79b35663a61815
Daniel Andersson <d.andersson86@icloud.com>
2020-10-08 12:30:30 +0200

    Fix database users table insert with example user password matching JWT password.

--- app.js
+++ app.js
@@ -8,40 +8,35 @@
 "use strict";
 
 /* --- Required Dependencies for Application --- */
-const bodyParser = require("body-parser"); // Require BodyParser
-const morgan = require("morgan"); // Require Morgan for third party logging
-const cors = require("cors"); // Require CORS (cross-origin resource sharing)
-
-/* --- Require Express --- */
 const express = require("express"); // Require Express
-const app = express(); // Create constant "app" to run Express through
-const port = 1337; // Set port 1337
+const bodyParser = require("body-parser"); // Require BodyParser
+const cors = require("cors"); // Require CORS (cross-origin resource sharing)
+const morgan = require("morgan"); // Require Morgan for third party logging
 
 /* --- Require database --- */
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./db/texts.sqlite');
 
-/* --- Insert to database --- */
-db.run("insert into users (email, password) values (?, ?)",
-    "superlongtpassword", (error) => {
-    if (error) {
-        console.log("Error, database insert fail.");
-        return error;
-    }
+/* --- Require BCryptJS --- */
+const bcrypt = require('bcryptjs');
+const saltRounds = 12;
+const myPlaintextPassword = 'longAndSuperHardP4$$wOrDplease';
+const hash = 'longAndSuperHardP4$$wOrDplease';
 
-    return console.info("Database insert success!");
-});
+/* --- Require jsonwebtoken --- */
+const jwt = require('jsonwebtoken');
 
-/* --- Application use CORS --- */
-app.use(cors());
-
-/* --- Required Routes --- */
+/* --- Require Routes --- */
 const indexRoute = require('./routes/index');
 const helloRoute = require('./routes/hello');
 
-/* --- Application use routes --- */
-app.use('/', indexRoute);
-app.use('/hello', helloRoute);
+/* --- Setup for enable to run Express with specific Port & router --- */
+const app = express(); // Create constant "app" to run Express through
+const router = express.Router(); // Create "router" constant for use of Express routes.
+const port = 1337; // Set port 1337
+
+/* --- Application use CORS --- */
+app.use(cors());
 
 /* --- Morgan setup to avoid logging 'test' --- */
 if (process.env.NODE_ENV !== 'test') {
@@ -59,6 +54,74 @@ app.use((request, response, next) => {
 app.use(bodyParser.json()); // enable parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // enable parsing application/x-www-form7urlencoded
 
+/* --- Application use routes --- */
+app.use('/', indexRoute);
+app.use('/hello', helloRoute);
+
+
+
+/* --- Insert to database --- */
+db.run("insert into users (email, password) values (?, ?)",
+    "d.andersson@example.com",
+    "longAndSuperHardP4$$wOrDplease", (error) => {
+        if (error) {
+            console.log("Error with database insert.");
+            console.log("Could be because user already exists i database.");
+            return error;
+        }
+
+        return console.info("Database insert success!");
+    });
+
+
+
+/* --- BCrypt save passwords to db --- */
+bcrypt.hash(myPlaintextPassword, saltRounds, function(error, hash) {
+    // Save passwords to database.
+});
+
+/* --- BCrypt compare password to database --- */
+bcrypt.compare(myPlaintextPassword, hash, function(error, response) {
+    // Response now contains true/false dependent on if its the right password or not.
+});
+
+
+
+/* --- JWT config-object --- */
+const payload = {
+    email: "d.andersson@example.com"
+};
+const secret = process.env.JWT_SECRET;
+const token = jwt.sign(payload, secret, {
+    expiresIn: '30min'
+});
+
+/* --- JWT Verify --- */
+jwt.verify(token, process.env.JWT_SECRET, function(error, decoded) {
+   if (error) {
+     console.log("Error to verify JWT Token. ", error);
+   }
+
+   console.log("JWT Token verified successfully. ", decoded);
+});
+
+/* --- Check JWT --- */
+router.post("/reports",
+    (request, response, next) => checkToken(request, response, next),
+    (request, response) => reports.addReport(response, request.body))
+
+function checkToken(request, response, next) {
+    const token = request.headers['x-access-token'];
+
+    jwt.verify(token, process.env.JWT_SECRET, function(error, decoded) {
+        if (error) {
+            // Send error response
+            console.log("Check Token Error: ", error);
+        }
+        next();
+    });
+}
+
 /* 404 - Route to catch errors on wrong routes */
 app.use((request, response, next) => {
     var error = new Error("Not Found");
@@ -72,15 +135,17 @@ app.use((error, request, response, next) => {
     }
 
     response.status(error.status || 500).json({
-        "errors": [
+        errors: [
             {
-                "status": error.status,
-                "title": error.message,
-                "detail": error.message
+                status: error.status,
+                title: error.message,
+                detail: error.message
             }
         ]
     });
 });
 
-// Start up server
+
+
+/* --- Start up server --- */
 app.listen(port, () => console.info(`API listening to port ${port}!`));
