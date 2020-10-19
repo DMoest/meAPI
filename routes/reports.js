506351b1fef3f7a9e22fdfda16d4492767aca345
Daniel Andersson <d.andersson86@icloud.com>
2020-10-19 09:49:07 +0200

    Add route for GET reports/kmom/:id

--- routes/reports.js
+++ routes/reports.js
@@ -1,7 +1,6 @@
 let express = require('express');
 let router = express.Router();
 
-
 router
     .get("/", function(request, response) {
         const data = {
@@ -10,17 +9,31 @@ router
             }
         };
 
-        return response.json(data);
+        console.log("REQ.BODY: ", request.body);
+        response.json(data);
     })
     .post("/", function(request, response) {
-    const data = {
-        data: {
-            "message": "Hello World! This is 'REPORTS' POST route."
-        }
-    };
+        const data = {
+            data: {
+                "message": "Hello World! This is 'REPORTS' POST route."
+            }
+        };
 
-    console.log(request.body);
-    return response.json(data);
-});
+        console.log("REQ.BODY: ", request.body);
+        response.json(data);
+    });
+
+router
+    .get("/kmom/:id", function(request, response) {
+        let id = request.params.id
+        const data = {
+            data: {
+                "message": `Report for Kmom ${id}.`
+            }
+        };
+
+        console.log("REQ.BODY: ", request.params);
+        response.json(data);
+    });
 
 module.exports = router;
